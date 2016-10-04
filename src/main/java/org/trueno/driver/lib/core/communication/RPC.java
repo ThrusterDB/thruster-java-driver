24f92931f5dcaf4fe95853735552b054dfdb9c54
Edgardo A Barsallo Yi <edgardo.barsallo@gmail.com>
2016-10-04 10:55:24 -0400

    Clean code

--- src/main/java/org/trueno/driver/lib/core/communication/RPC.java
+++ src/main/java/org/trueno/driver/lib/core/communication/RPC.java
@@ -3,16 +3,20 @@ package org.trueno.driver.lib.core.communication;
 import com.github.nkzawa.socketio.client.Ack;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
+import org.jdeferred.Deferred;
+import org.jdeferred.Promise;
+import org.jdeferred.impl.DeferredObject;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.lang.reflect.Method;
-import java.util.concurrent.CompletableFuture;
 
 /**
- * Created by: victor
+ * @author Victor Santos
+ * @author Miguel Rivera
+ *
  * Date: 7/19/16
  * Purpose:
  */
@@ -51,32 +55,39 @@ public class RPC {
         this.procedures.put(procedureName, procedureFunction);
     }
 
-    public CompletableFuture<JSONObject> call(final String method, final JSONObject arg) {
-        return CompletableFuture.supplyAsync(() -> {
-            JSONObject value = new JSONObject();
+    public Promise<JSONObject, JSONObject, Integer> call(final String method, final JSONObject arg) {
 
-            /* Sending event */
-            this.socket.emit(method, arg, (Ack) objects -> {
+        /* Instantiating deferred object */
+        final Deferred<JSONObject, JSONObject, Integer> deferred = new DeferredObject<>();
+
+        /* Extracting promise */
+        Promise<JSONObject, JSONObject, Integer> promise = deferred.promise();
+
+        /* Sending event */
+        this.socket.emit(method, arg, (Ack) objects -> {
 
             /* casting json object */
-                JSONObject args = (JSONObject) objects[0];
+            JSONObject args = (JSONObject) objects[0];
 
             /* checking the result and resolving or rejecting */
-                try {
-                    value.put("result", args.get("_payload"));
+            try {
+                JSONObject value = new JSONObject();
+                value.put("result", args.get("_payload"));
 
-                    if (args.get("_status").toString().equals(Status.ERROR.toString())) {
-                        throw new Error("An error occurred while retrieving data: " + value);
-                    }
-
-                } catch (JSONException e) {
-                    throw new RuntimeException("An error occurred while manipulating JSON message", e);
+                if (args.get("_status").toString().equals(Status.SUCCESS.toString())) {
+                    deferred.resolve(value);
+                } else if (args.get("_status").toString().equals(Status.ERROR.toString())) {
+                    deferred.reject(value);
                 }
-            });
 
-            return value;
+            } catch (JSONException e) {
+                throw new RuntimeException("An error occurred while manipulating JSON message", e);
+            }
         });
+
+        return promise;
     }
+
 
     public void connect(final Callback connCallback, final Callback discCallback) {
         /* instantiating the socket */
@@ -86,7 +97,9 @@ public class RPC {
             throw new Error("Could not connect to the database", e);
         }
 
-        this.socket.on(Socket.EVENT_CONNECT, args -> connCallback.method(this.socket)).on(Socket.EVENT_DISCONNECT, args -> discCallback.method(this.socket));
+        this.socket
+                .on(Socket.EVENT_CONNECT,    args -> connCallback.method(this.socket))
+                .on(Socket.EVENT_DISCONNECT, args -> discCallback.method(this.socket));
 
         /* Connecting Socket */
         this.socket.connect();
